9e945e02fb81136425d5e65ada9ade1693dca6a2
Calvin Xu <calvinxu806@gmail.com>
2022-04-02 20:44:05 -0700

    IO setup & some high level organization laid out

--- hachip.c
+++ hachip.c
@@ -1,3 +1,53 @@
 #include "hachip.h"
+#include "gl.h"
+#include "interrupts.h"
+#include "keyboard.h"
+#include "malloc.h"
+#include "ps2.h"
+#include "roms.h"
 
-int main() { return 0; }
+#define DISPLAY_WIDTH 64
+#define DISPLAY_HEIGHT 32
+
+void init_keyboard(void) {
+  gpio_init();
+  keyboard_init(KEYBOARD_CLOCK, KEYBOARD_DATA);
+  // https://wiki.osdev.org/%228042%22_PS/2_Controller#PS.2F2_Controller_Commands
+  ps2_device_t *keyboard = ps2_new(KEYBOARD_CLOCK, KEYBOARD_DATA);
+  while (true) {
+    if (keyboard_read_scancode() == 0x55) {
+      ps2_write(keyboard, 0x55);
+      break;
+    }
+  }
+  free(keyboard);
+  interrupts_init();
+  keyboard_use_interrupts();
+  interrupts_global_enable();
+}
+
+void init_display(unsigned int width, unsigned int height) {
+  gl_init(width, height, GL_DOUBLEBUFFER);
+  gl_clear(GL_BLACK);
+}
+
+void init_chip(void);
+
+void load_program(unsigned short *);
+
+void emulate_cycle(void);
+
+void draw_graphics(void);
+
+void set_keys(void);
+
+int main() {
+  init_keyboard();
+  init_display(DISPLAY_WIDTH, DISPLAY_HEIGHT);
+  load_program(TEST_ROM);
+  while (true) {
+    emulate_cycle();
+    set_keys();
+  }
+  return 0;
+}
--- hachip.h
+++ hachip.h
@@ -1,9 +1,11 @@
 #ifndef HACHIP_H
 #define HACHIP_H
+#include <stdbool.h>
 
-unsigned short opcode;
-
+// https://tobiasvl.github.io/blog/write-a-chip-8-emulator/#stack
+// https://multigesture.net/articles/how-to-write-an-emulator-chip-8-interpreter/
 struct {
+  unsigned short opcode;
   // 4 kilobytes of RAM
   // 0x000-0x1FF - interpreter & font
   // 0x200-0xFFF - program & RAM
@@ -19,12 +21,28 @@ struct {
   unsigned short STACK[16];
   unsigned short SP;
   // pixel states (64 * 32)
-  unsigned char (*pixels)[64];
+  bool (*pixels)[64];
   // hex keypad
   unsigned char keypad[16];
   // two timer registers that decrement at 60 Hz
   unsigned char delay_timer;
   unsigned char sound_timer;
+  // set when graphics need to be redrawn
+  bool draw_flag;
 } chip;
+
+void init_keyboard(void);
+
+void init_display(unsigned int width, unsigned int height);
+
+void init_chip(void);
+
+void load_program(unsigned short *);
+
+void emulate_cycle(void);
+
+void draw_graphics(void);
+
+void set_keys(void);
 
 #endif
