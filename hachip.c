13b336faaf79607c782787d83a61c700cece21ae
Calvin Xu <calvinxu806@gmail.com>
2022-04-04 14:28:47 -0700

    Initialization of chip memory and registers

--- hachip.c
+++ hachip.c
@@ -5,9 +5,7 @@
 #include "malloc.h"
 #include "ps2.h"
 #include "roms.h"
-
-#define DISPLAY_WIDTH 64
-#define DISPLAY_HEIGHT 32
+#include "strings.h"
 
 void init_keyboard(void) {
   gpio_init();
@@ -31,11 +29,28 @@ void init_display(unsigned int width, unsigned int height) {
   gl_clear(GL_BLACK);
 }
 
-void init_chip(void);
+#define FONT_START 0x50
+void init_chip(void) {
+  CHIP.OPCODE = 0;
+  CHIP.PC = 0x200;
+  CHIP.I = 0;
+  CHIP.SP = 0;
+  memset(CHIP.MEM, 0, sizeof(CHIP.MEM));
+  memset(CHIP.V, 0, sizeof(CHIP.V));
+  memset(CHIP.STACK, 0, sizeof(CHIP.STACK));
+  for (int i = 0; i < DISPLAY_HEIGHT; i++) {
+    memset(CHIP.PIXELS + i, 0, DISPLAY_WIDTH);
+  }
+  for (int i = 0; i < 80; i++) {
+    CHIP.MEM[i + FONT_START] = font[i];
+  }
+}
 
-void load_program(unsigned short *);
+void load_program(unsigned short *program) {
+  memcpy(CHIP.MEM + 0x200, &program, sizeof(program));
+}
 
-void emulate_cycle(void);
+void emulate_cycle(void) {}
 
 void draw_graphics(void);
 
@@ -44,7 +59,8 @@ void set_keys(void);
 int main() {
   init_keyboard();
   init_display(DISPLAY_WIDTH, DISPLAY_HEIGHT);
-  load_program(TEST_ROM);
+  init_chip();
+  load_program(IBM_LOGO);
   while (true) {
     emulate_cycle();
     set_keys();
--- hachip.h
+++ hachip.h
@@ -2,10 +2,16 @@
 #define HACHIP_H
 #include <stdbool.h>
 
+#define DISPLAY_WIDTH 64
+#define DISPLAY_HEIGHT 32
+
+#define PHYSICAL_WIDTH 64
+#define PHYSICAL_HEIGHT 32
+
 // https://tobiasvl.github.io/blog/write-a-chip-8-emulator/#stack
 // https://multigesture.net/articles/how-to-write-an-emulator-chip-8-interpreter/
-struct {
-  unsigned short opcode;
+typedef struct {
+  unsigned short OPCODE;
   // 4 kilobytes of RAM
   // 0x000-0x1FF - interpreter & font
   // 0x200-0xFFF - program & RAM
@@ -21,15 +27,17 @@ struct {
   unsigned short STACK[16];
   unsigned short SP;
   // pixel states (64 * 32)
-  bool (*pixels)[64];
+  bool (*PIXELS)[64];
   // hex keypad
-  unsigned char keypad[16];
+  unsigned char KEYPAD[16];
   // two timer registers that decrement at 60 Hz
-  unsigned char delay_timer;
-  unsigned char sound_timer;
+  unsigned char DELAY_TIMER;
+  unsigned char SOUND_TIMER;
   // set when graphics need to be redrawn
-  bool draw_flag;
-} chip;
+  bool DRAW_FLAG;
+} chip_t;
+
+chip_t CHIP;
 
 void init_keyboard(void);
 
@@ -44,5 +52,24 @@ void emulate_cycle(void);
 void draw_graphics(void);
 
 void set_keys(void);
+
+unsigned char font[80] = {
+    0xF0, 0x90, 0x90, 0x90, 0xF0, // 0
+    0x20, 0x60, 0x20, 0x20, 0x70, // 1
+    0xF0, 0x10, 0xF0, 0x80, 0xF0, // 2
+    0xF0, 0x10, 0xF0, 0x10, 0xF0, // 3
+    0x90, 0x90, 0xF0, 0x10, 0x10, // 4
+    0xF0, 0x80, 0xF0, 0x10, 0xF0, // 5
+    0xF0, 0x80, 0xF0, 0x90, 0xF0, // 6
+    0xF0, 0x10, 0x20, 0x40, 0x40, // 7
+    0xF0, 0x90, 0xF0, 0x90, 0xF0, // 8
+    0xF0, 0x90, 0xF0, 0x10, 0xF0, // 9
+    0xF0, 0x90, 0xF0, 0x90, 0x90, // A
+    0xE0, 0x90, 0xE0, 0x90, 0xE0, // B
+    0xF0, 0x80, 0x80, 0x80, 0xF0, // C
+    0xE0, 0x90, 0x90, 0x90, 0xE0, // D
+    0xF0, 0x80, 0xF0, 0x80, 0xF0, // E
+    0xF0, 0x80, 0xF0, 0x80, 0x80  // F
+};
 
 #endif
